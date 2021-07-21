e4a0f8f30d63e447247b3cf33ad174855b14d994
Imanuel <chaturi@sheridancollege.ca>
2021-07-21 12:34:55 -0400

    Game runs except for no error handling, or a win condition checker.

--- src/ca/sheridancollege/project/CardGame.java
+++ src/ca/sheridancollege/project/CardGame.java
@@ -48,33 +48,45 @@ public class CardGame {
 
                 pList[i].hand.add(new Card(randVal, randSuit));
                 System.out.printf("%s of %s\n", pList[i].hand.get(x).getValue(),
-                         pList[i].hand.get(x).getSuit());
+                        pList[i].hand.get(x).getSuit());
             }
         }
         //game loop
         while (game) {
             for (int i = 0; i < players; i++) {
+                int cardsGotten = 0;
                 //Display-------------------------------------------------------
                 System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n"
                         + "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
                 System.out.println("your hand...");
-                
+
                 for (Card c : pList[i].hand) {
                     System.out.printf("%s OF %s\n", c.getValue(), c.getSuit());
                 }
                 //--------------------------------------------------------------
-                
-                
-                System.out.println("Player " + players + ", Who do you wanna ask?");
+
+                System.out.println("Player " + (i + 1) + ", Who do you wanna ask?");
                 int askPlayer = input.nextInt() - 1;
                 System.out.println("What number are you looking for?");
                 Value askVal = Value.valueOf(input.next().toUpperCase());
-                
-                for(int x=0; x < pList[askPlayer].hand.size(); x++){
+
+                for (int x = 0; x < pList[askPlayer].hand.size(); x++) {
                     System.out.println(pList[askPlayer].hand.size());
-                    if(pList[askPlayer].hand.get(x).getValue().equals(askVal)){
-                        System.out.println("aaaaa end me");
+                    if (pList[askPlayer].hand.get(x).getValue().equals(askVal)) {
+                        cardsGotten++;
+                        Card takeCard = pList[askPlayer].hand.get(x);
+                        pList[askPlayer].hand.remove(x);
+                        pList[i].hand.add(takeCard);
+
                     }
+                }
+                if (cardsGotten == 0) {
+                    int randCard = (int) (Math.random() * deck.size());
+                    Value randVal = deck.get(randCard).getValue();
+                    Suit randSuit = deck.get(randCard).getSuit();
+                    deck.remove(randCard);
+                    pList[i].hand.add(new Card(randVal, randSuit));
+
                 }
             }
         }
